86b25e72ebba212a8eb6614af7119cf3bdd6d5e3
biobits <email@biobits.eu>
2020-02-03 15:45:38 +0000

    Added kmsurv based on survminer

--- bbsurvr/R/survival.R
+++ bbsurvr/R/survival.R
@@ -4,15 +4,16 @@
 ##########################################################################################################################################################################################################
 #' Function to calculate the probability of survival for a point in time
 #'
-#'
-#' @param S
-#' @param totaltimes
+#' @param S xx
+#' @param totaltimes xx
 #' @return one vector with the first non-NA value
 #'
 #' @author Stefan Bartels, \email{email@biobits.eu}
 #'
 #' @examples
+#' \dontrun{
 #' x<-kmsurv(S,totaltimes)
+#' }
 #'
 #'@export
 kmsurv <- function(S, totaltimes) {
@@ -40,7 +41,9 @@ kmsurv <- function(S, totaltimes) {
 #' @author Stefan Bartels, \email{email@biobits.eu}
 #'
 #' @examples
+#' \dontrun{
 #' pvalue<-p.value.survdiff(daten=df,group="Treatment",time="duration",status="survivalstatus")
+#' }
 #'
 #'@export
 p.value.survdiff<-function(daten,group=NULL, time="time",status="status")
@@ -52,10 +55,206 @@ p.value.survdiff<-function(daten,group=NULL, time="time",status="status")
 }
 
 ##########################################################################################################################################################################################################
+##Funktion f?r eine zum erstellen eines einheitlichen BoxPlots
+##########################################################################################################################################################################################################
+#' R Function to streamline the generation of (grouped) boxplots
+#'
+#'
+#' @param daten the vector of numeric data
+#' @param gruppe if given the factors to be grouped by
+#' @param filename if given output will be delivered to file
+#' @param ylab label of y axis
+#' @param titel
+#'
+#' @return a base boxplot
+#'
+#' @import bbhelper
+#'
+#' @author Stefan Bartels, \email{email@biobits.eu}
+#'
+#' @examples
+#' bp<-bbBoxPlot(iris$Petal.Width,gruppe=iris$Species,ylab="Petal Width")
+#'
+#'@export
+bbBoxPlot<- function(daten,gruppe=NULL,filename=NULL,ylab,titel=NULL)
+{
+
+  mypalette<-bbhelper::getBBColors(length(levels(gruppe)))
+  if (is.null(filename)==FALSE){png(file=paste(c(filename,".png"),collapse = ""),bg="white",res=300,width=1600,height=1600) }
+  boxplot(daten ~ gruppe,ylab=ylab,col=mypalette,  horizontal=FALSE,cex.axis=0.7, las=3,show.names=FALSE)
+  legend("topright",levels(gruppe),fill=mypalette,cex=0.8)
+  if (is.null(titel)==FALSE)
+  {
+    title(main = list(titel,cex= 0.8, col="#60759B"))
+  }
+  text(0.2,0,paste(" created by biobits\n",Sys.Date()),col="grey",pos=4,cex=0.6)
+  if (is.null(filename)==FALSE){dev.off()}
+}
+
+##########################################################################################################################################################################################################
+##Funktion f?r das erzeugen eines angepassten RGB-GmbH Farbthemas
+##########################################################################################################################################################################################################
+#' R fuction to set a unified layout for base plots
+#'
+#' accepts a list of vectors of identical length and returns one vector with the first non-NA value
+#'
+#'
+#' @author Stefan Bartels, \email{email@biobits.eu}
+#'
+#' @examples
+#' \dontrun{
+#' bbTheme()
+#' }
+#'
+#'@export
+bbTheme <- function() {
+  par <- col.whitebg()
+  par$strip.background$col <- rep("#60759B", 7)
+  par$add.text$col <- "#eeeeaa"
+  par$add.text$font <- 2
+  par$background$col <- "#ffffff"
+  par$superpose.line$lty <- rep(1, 7)
+  par$superpose.line$col[1:2] <- c("#880000", "#008800")
+  par$superpose.symbol$col[1:2] <- c("#880000", "#008800")
+  par
+}
+
+##########################################################################################################################################################################################################
+## Function to streamline survival plots on base of survminer
+##########################################################################################################################################################################################################
+#' R Function to streamline the generation of survival plots
+#'
+#' accepts a data frame containing survival data an
+#'
+#' @param daten the data.frame with survival data
+#' @param gruppe optional: the factor the plot has to be grouped by
+#' @param time optional: data holding the the time
+#' @param status optional: data holding survival status (0/1)
+#' @param titel the title of the plot
+#' @param survtime the time intervall in month to show the survival rate for (e.g. 24 for 2-year survival-rate)
+#' @param survtimetext the text to label the survival rate
+#' @param risk.table if TRUE the the risk table is ploted beneath the graph. Defalut is true.
+#' @param showmedian if true the median value is shown in plot /legend (for more than one group). Default is true.
+#' @param xmax MAx Value for X-axis
+#' @param xlab Label for x-axis (default="Monate (Anzeige bis max. 5 Jahre)")
+#' @param cex.lab Fontsize for label (default=1)
+#' @param cex.axis Fontsize for axis (default=1)
+#' @param watermark if TRUE the biobits watermark will be printet on plot
+#' @param ylab the label for y-axis
+#' @param logrank if true a logrank test is performed  and the p-value will be printet on the plot (default=FALSE)
+#'
+#'
+#' @return a survival plot
+#'
+#' @import survminer survival bbhelper dplyr ggplot2 ggpubr
+#'
+#' @author Stefan Bartels, \email{email@biobits.eu}
+#'
+#' @examples
+#' \dontrun{
+#' data("myeloma")
+#' bbggkmplot(daten = myeloma ,time=time
+#'           ,gruppe = molecular_group
+#'           ,status=event,logrank=T
+#'           ,watermark = T,risk.table = T
+#'           ,showmedian = T
+#'           ,survtime=60
+#'           ,survtimetext="5-Y SR")
+#'           }
+#'
+#'@export
+bbggkmplot<-function(daten,gruppe=NULL,time=time,status=status,xlab="Time in months"
+                     ,cex.lab=1,cex.axis=1,watermark=TRUE,ylab="",title="",survtime=NULL,survtimetext=NULL
+                     ,risk.table  = TRUE,logrank=FALSE,xmax=100,showmedian=T, ...){
+  qTIME <- dplyr::enquo(time)                    # Create quosure
+  qSTATUS  <- dplyr::enquo(status)               # Create quosure
+  qLevels<-1
+  qMedian<-"none"
+  qLegend<-"right"
+  if(!missing(gruppe)){
+    qGRUPPE <- dplyr::enquo(gruppe)
+    Sdata<-daten%>%dplyr::select(time= !! dplyr::as_label(qTIME),status= !! dplyr::as_label(qSTATUS),group= !! dplyr::as_label(qGRUPPE))
+    Sdata$group<-as.factor(Sdata$group)
+    fit <- survival::survfit(survival::Surv(time=time, event=status) ~ group, data=Sdata)
+    qLevels<-length(levels(Sdata$group))
+    groupnames<-levels(Sdata$group)
+    if (showmedian){
+      gMedian<-as.vector(survminer::surv_median(fit)["median"])
+      medianText<-paste0("\nMedian = ", as.vector(unlist(gMedian)))
+      groupnames<-paste0(groupnames,medianText)
+    }
+  }else{
+    Sdata<-daten%>%dplyr::select(time= !! dplyr::as_label(qTIME),status= !! dplyr::as_label(qSTATUS))
+    fit <- survival::survfit(survival::Surv(time=time, event=status)~1 , data=Sdata)
+    groupnames<-""
+    if(showmedian){
+      qMedian<-"hv"
+    }
+    qLegend<-"none"
+  }
+
+  mypalette<-bbhelper::getBBColors(qLevels)
+
+  #survivalprobability at timepoint
+  subtext<-""
+  if(!is.null(survtime)){
+    survtimepoint<-formatC((summary(fit, times=survtime)$surv),digits=2,format="f")
+    # Wenn wir nur eine Gruppe haben Angabe als "caption"
+    if(missing(gruppe)){
+      subtext<-paste("\n",survtimetext," = ",survtimepoint)
+    }else{
+      groupnames<-paste0(groupnames,paste("\n",survtimetext," = ",survtimepoint,"\n"))
+    }
+  }
+  survplot<-survminer::ggsurvplot(
+    fit, # fitted survfit object
+    data=Sdata,
+    risk.table  = risk.table, # include risk table?
+    conf.int    = TRUE, # add confidence intervals?
+    pval        = logrank, # add p-value to the plot?
+    pval.size=3,
+    title=title,
+    break.time.by = 12, #default because we mostly use months
+    legend=qLegend,
+    xlim = c(0, xmax),
+    xlab = xlab,
+    caption  =subtext,
+    show.legend = FALSE,
+    ylab=ylab,
+    risk.table.col = "strata",
+    risk.table.fontsize=3,
+    palette=mypalette,
+    ggtheme = theme_light(),
+    legend.title = "",
+    surv.median.line = qMedian, # median survival in plot
+    conf.int.style = "step",  # customize style of confidence intervals
+    risk.table.y.text = FALSE,# show bars instead of names in text annotations
+    # in legend of risk table.
+    legend.labs = groupnames,
+    font.x       = c(8), #  font for the x axis
+    font.legend = c(8),
+    font.caption =c(7)
+  )
+  if (watermark){
+    #
+    survplot$plot<- survplot$plot + ggplot2::annotate("text", x = Inf, y = -Inf,
+                                                      label = paste(" created by biobits\n",Sys.Date()),
+                                                      hjust=1.1, vjust=-0.2, col="grey", cex=2.5, alpha = 0.8)
+  }
+  if(risk.table){
+    survplot$table <- ggpubr::ggpar(
+      survplot$table,
+      font.title    = c(8),
+      font.x        = c(8),
+      font.xtickslab = c(8)
+    )
+  }
+  survplot
+}
+
+
+##########################################################################################################################################################################################################
 ##Funktion zum erstellen eines einheitlichen KM-Plots
-##
-## Hinweis: Bitte unbeding das Paket "rms" mit seiner funktion "survplot" prüfen
-##
 ##########################################################################################################################################################################################################
 #' R Function to streamline the generation of survival plots
 #'
@@ -87,17 +286,19 @@ p.value.survdiff<-function(daten,group=NULL, time="time",status="status")
 #' @author Stefan Bartels, \email{email@biobits.eu}
 #'
 #' @examples
+#' \dontrun{
 #' p<-bbKMPlot(daten,gruppe=NULL,titel,filename,survtime=NULL,survtimetext=NULL,legendeout=NULL,file.out=TRUE,legfontsize=NULL,subtext="",
 #' xmax=61.25,xlab="Beobachtungszeit in Monaten",cex.lab=1,cex.axis=1,watermark=TRUE,ylab="",logrank=FALSE)
+#' }
 #'
 #'@export
 bbKMPlot <- function (daten,gruppe=NULL,titel,filename,survtime=NULL,survtimetext=NULL,legendeout=NULL,file.out=TRUE,legfontsize=NULL,subtext="",
-                       xmax=61.25,xlab="Beobachtungszeit in Monaten",cex.lab=1,cex.axis=1,watermark=TRUE,ylab="",logrank=FALSE)
+                      xmax=61.25,xlab="Beobachtungszeit in Monaten",cex.lab=1,cex.axis=1,watermark=TRUE,ylab="",logrank=FALSE)
 {
   #status als numeric definieren
   daten$status<-as.numeric(daten$status)
   blwd<-2
-  #Schriftgröße der Legende
+  #Schriftgroesse der Legende
   if (is.null(legfontsize)==TRUE){leg.cex<-0.6} else {leg.cex<-legfontsize}
 
   if ((is.null(gruppe)==TRUE) || ((length(levels(as.factor(daten[[gruppe]])))==1)))
@@ -220,69 +421,9 @@ bbKMPlot <- function (daten,gruppe=NULL,titel,filename,survtime=NULL,survtimetex
     {dev.off()}
   }
 }
-##########################################################################################################################################################################################################
-##Funktion f?r eine zum erstellen eines einheitlichen BoxPlots
-##########################################################################################################################################################################################################
-#' R Function to streamline the generation of (grouped) boxplots
-#'
-#'
-#' @param daten the vector of numeric data
-#' @param gruppe if given the factors to be grouped by
-#' @param filename if given output will be delivered to file
-#' @param ylab label of y axis
-#' @param titel
-#'
-#' @return a base boxplot
-#'
-#' @import bbhelper
-#'
-#' @author Stefan Bartels, \email{email@biobits.eu}
-#'
-#' @examples
-#' bp<-bbBoxPlot(daten)
-#' bp<-bbBoxPlot(iris$Petal.Width,gruppe=iris$Species,ylab="Petal Width")
-#'
-#'@export
-bbBoxPlot<- function(daten,gruppe=NULL,filename=NULL,ylab,titel=NULL)
-{
-
-  mypalette<-bbhelper::getBBColors(length(levels(gruppe)))
-  if (is.null(filename)==FALSE){png(file=paste(c(filename,".png"),collapse = ""),bg="white",res=300,width=1600,height=1600) }
-  boxplot(daten ~ gruppe,ylab=ylab,col=mypalette,  horizontal=FALSE,cex.axis=0.7, las=3,show.names=FALSE)
-  legend("topright",levels(gruppe),fill=mypalette,cex=0.8)
-  if (is.null(titel)==FALSE)
-  {
-    title(main = list(titel,cex= 0.8, col="#60759B"))
-  }
-  text(0.2,0,paste(" created by biobits\n",Sys.Date()),col="grey",pos=4,cex=0.6)
-  if (is.null(filename)==FALSE){dev.off()}
-}
 
 
 
-##########################################################################################################################################################################################################
-##Funktion f?r das erzeugen eines angepassten RGB-GmbH Farbthemas
-##########################################################################################################################################################################################################
-#' R fuction to set a unified layout for base plots
-#'
-#' accepts a list of vectors of identical length and returns one vector with the first non-NA value
-#'
-#'
-#' @author Stefan Bartels, \email{email@biobits.eu}
-#'
-#' @examples
-#' bbTheme()
-#'
-#'@export
-bbTheme <- function() {
-  par <- col.whitebg()
-  par$strip.background$col <- rep("#60759B", 7)
-  par$add.text$col <- "#eeeeaa"
-  par$add.text$font <- 2
-  par$background$col <- "#ffffff"
-  par$superpose.line$lty <- rep(1, 7)
-  par$superpose.line$col[1:2] <- c("#880000", "#008800")
-  par$superpose.symbol$col[1:2] <- c("#880000", "#008800")
-  par
-}
+
+
 
